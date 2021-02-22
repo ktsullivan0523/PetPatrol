c0242adb9d09617ff22c073366b6fd88a5af1a1a
ktsullivan0523 <61055398+ktsullivan0523@users.noreply.github.com>
2021-02-22 11:39:44 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './apps/screens/SignInScreen';
+
+export default function App() {
+  return (
+    <SignInScreen />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
